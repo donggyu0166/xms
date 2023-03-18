971cc40a42586fa581852fd16eec0d38050f2837
KDG <KDG@DESKTOP-2D9SANP>
2023-03-18 15:25:47 +0900

    init

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str, sword;
+		Scanner sc = new Scanner (System.in);
+		str = sc.nextLine (); // Read a line
+		System.out.println ("str = " + str);
+
+}
