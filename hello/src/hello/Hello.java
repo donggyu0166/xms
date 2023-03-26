297c8cafe38df61dd9bd0b3b5a98687ba7cf8fa2
donggyu0166 <127824217+donggyu0166@users.noreply.github.com>
2023-03-27 02:52:07 +0900

    Update Hello.java

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -4,10 +4,31 @@ import java.util.Scanner;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("1. Add Member");
-		System.out.println("2. Delete Member");
-		System.out.println("3. Edit Date");
-		System.out.println("4. View Date Remaining");
-		System.out.println("5. Exit");
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num != 6) {
+			System.out.println("1. Add Member");
+			System.out.println("2. Delete Member");
+			System.out.println("3. Edit Date");
+			System.out.println("4. View Date Remaining");
+			System.out.println("5. Show a menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1 - 6: ");
+			num = input.nextInt();
+			
+			switch(num) {
+			case 1:
+				System.out.print("Member name:");
+				String name = input.next();
+				System.out.print("Member Phone number:");
+				int number = input.nextInt();
+				break;
+				
+			case 2:
+			case 3:
+			case 4:
+			}
+		}
 	}
 }
